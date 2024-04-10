43b3651e81972eecd8b061e77bfcc29dc990409d
Ruth Georgiev <ruth.georgiev@proton.me>
2024-04-10 15:52:31 +0200

    new change on 10-april

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
+
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 
